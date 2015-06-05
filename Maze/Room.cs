ec773e6254ca967d926632638510ad74edee5775
dcmaysse <david.may@live.mercer.edu>
2015-06-05 12:45:19 +0900

    Room finished, Maze started

---
+++ Maze/Room.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maze
+{
+    public class Room
+    {
+        private Boolean exit;
+        private Boolean visited;
+        private Room parent;
+        private Room[] children;
+
+        public Room(Boolean exit)
+        {
+            this.exit = exit;
+            visited = false;
+            parent = null;
+            children = new Room[3];
+        }
+
+        public void addChild(Room child,int pos)
+        {
+            children[pos] = child;
+            child.setParent(this);
+        }
+
+        public void setParent(Room parent)
+        {
+            this.parent = parent;
+        }
+
+        public void visit()
+        {
+            visited = true;
+        }
+
+        public Room getParent()
+        {
+            return parent;
+        }
+
+        public Room getChild(int pos)
+        {
+            children[pos].visit();
+            return children[pos];
+        }
+
+        public Boolean isVisited()
+        {
+            return visited;
+        }
+
+        public Boolean isExit()
+        {
+            return exit;
+        }
+    }
+}
